c7842d6c840ce1cc766a925cb5154612bcd41c5e
Fakorede Oluwaleke <hashilekky@gmail.com>
2022-05-09 17:26:45 +0100

    Codebase setup

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,7 @@ export const dbTables = {
   organizations: 'organizations',
   admins: 'admins',
 };
+// Will make env variables later.
 export const ADMIN_ADDRESSES: string[] = ['0x22a8cd3a7a07527b3447eb42db073342b36c1d49']; // Fill up with permitted admin wallet addresses
 export const JWT_PUBLIC_KEY = <string> process.env.JWT_PUBLIC_KEY;
 export const nftTypes = ['AVATAR', 'ACCESSORY', 'GAME_ITEM'];
