14b0d7e865d513d9c9718f349ff4a9356e4af6ac
Fakorede Oluwaleke Adedoyin <hashilekky@gmail.com>
2022-06-07 20:23:36 +0100

    Changed admin address env (#8)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,7 @@ export const dbTables = {
   firstPartyQuestions: 'first_party_questions',
 };
 // Will make env variables later.
-export const ADMIN_ADDRESSES: string[] = ['0x22a8cd3a7a07527b3447eb42db073342b36c1d49']; // Fill up with permitted admin wallet addresses
+export const ADMIN_ADDRESSES: string[] = (<string> process.env.ADMIN_ADDRESSES || '').split(','); // Fill up with permitted admin wallet addresses
 export const JWT_PUBLIC_KEY = <string> process.env.JWT_PUBLIC_KEY;
 export const nftTypes = ['AVATAR', 'ACCESSORY', 'GAME_ITEM'];
 export const tokenFormats = ['ERC721', 'ERC1155'];
